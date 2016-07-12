63dc900157b46c4ad1d97b077cfa00e35e45bc8c
Mark-Nelson <mark.nelson@dragonicity.com>
2016-07-12 13:49:17 +0800

    Show user articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,30 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome tothe Alpha Blog #{@user.username}"
+      flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to articles_path
     elsif
       render 'new' 
     end    
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    
   end
 
   private
